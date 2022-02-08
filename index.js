5f0c1b0fb8c9c5ee76bde6be508ab05dd7a36acb
Surya Pratap Singh <suryaprataps424@gmail.com>
2022-02-08 12:27:54 +0530

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
-const fs = require('fs')
+const fs = require("fs");
 const app = express();
-
-app.get('/',(req,res)=>{
-    file = fs.readFileSync('./names.html','utf-8')
-    file = file.replace('{name}',req.query.name)
-    if(req.query.theme=='love')
-    file = file.replace('{theme}',"'I ❤️ You', '💕', '💓', '🧸'")
-    else if(req.query.theme=='birthday')
-    file = file.replace('{theme}',"'Happy BirthDay', '🎂', '🍰','🍥','🍬','🍫'")
-    else if(req.query.theme=='sorry')
-    file = file.replace('{theme}',"'Sorry', '🙃', '🥺','😭','😓','😥'")
-    else
-    file = 'please select a theme'
-    res.send(file)
-})
-app.listen(process.env.PORT||80, () => console.log("running"));
+app.use(express.static("public"));
+app.get("/page", (req, res) => {
+  file = fs.readFileSync("./names.html", "utf-8");
+  file = file.replace("{name}", req.query.name);
+  if (req.query.theme == "love")
+    file = file.replace("{theme}", "'I ❤️ You', '💕', '💓', '🧸'");
+  else if (req.query.theme == "birthday")
+    file = file.replace(
+      "{theme}",
+      "'Happy BirthDay', '🎂', '🍰','🍥','🍬','🍫'"
+    );
+  else if (req.query.theme == "sorry")
+    file = file.replace("{theme}", "'Sorry', '🙃', '🥺','😭','😓','😥'");
+  else file = "please select a theme";
+  res.send(file);
+});
+app.listen(process.env.PORT || 80, () => console.log("running"));
